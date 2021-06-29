18bd4a07e69b954c62985d1bb93e483aa131d20a
Raghed ALI <Raghed.Ali@sigma.se>
2021-06-29 21:06:52 +0200

    Module_4
    Signed-off-by: Raghed ALI <Raghed.Ali@sigma.se>

--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const multer = require('multer');
 const path = require('path');
+const imageProcessor = require('./imageProcessor');
 
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
@@ -28,18 +29,24 @@ const upload = multer({
     storage
 })
 
-router.get('/photo-viewer', (request, response) => {
-    response.sendFile(photoPath);
-})
-
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
     if(request.fileValidatorError){
         return response.status(400).json(
             {error: request.fileValidatorError});
     }
+
+    try {
+        await imageProcessor(request.file.filename);
+    } catch (error) {
+        
+    }
     return response.status(201).json(
         {success: true});
 });
+
+router.get('/photo-viewer', (request, response) => {
+    response.sendFile(photoPath);
+})
 
 module.exports = router;
 
